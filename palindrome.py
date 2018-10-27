e13312448d0bed0e255f3e9785e895b3fc1f312f
aishwaryaarangavittal <43928798+aishwaryaarangavittal@users.noreply.github.com>
2018-10-27 23:28:03 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,30 @@
+s1=input()
+s2=input()
+s3=input()
+s4=input()
+s5=input()
+s6=input()
+s7=input()
+s8=input()
+s9=input()
+s10=input()
+s11=input()
+s12=input()
+c1=s1+s2+s3+s4
+c2=s5+s6+s7+s8
+c3=s9+s10+s11+s12
+print(c1)
+print(c2)
+print(c3)
+if(c1==c1[::-1]):
+	print("1st is a palindrome")
+else:
+	print("1st is not a palindrome")
+if(c2==c2[::-1]):
+	print("2nd is a palindrome")
+else:
+	print("2nd is not a palindrome")
+if(c3==c3[::-1]):
+	print("3rd is a palindrome")
+else:
+	print("3rd is not a palindrome")
