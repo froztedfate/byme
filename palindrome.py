0cb7bf09793417b35bc0781a24119a81c2086da8
Varun Nivas <froztedfate@gmail.com>
2018-10-31 22:33:04 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -10,21 +10,21 @@ s9=input()
 s10=input()
 s11=input()
 s12=input()
-c1=s1+s2+s3+s4
-c2=s5+s6+s7+s8
-c3=s9+s10+s11+s12
-print(c1)
-print(c2)
-print(c3)
-if(c1==c1[::-1]):
+a1=s1+s2+s3+s4
+a2=s5+s6+s7+s8
+a3=s9+s10+s11+s12
+print(a1)
+print(a2)
+print(a3)
+if(a1==a1[::-1]):
 	print("1st is a palindrome")
 else:
 	print("1st is not a palindrome")
-if(c2==c2[::-1]):
+if(a2==a2[::-1]):
 	print("2nd is a palindrome")
 else:
 	print("2nd is not a palindrome")
-if(c3==c3[::-1]):
+if(a3==a3[::-1]):
 	print("3rd is a palindrome")
 else:
 	print("3rd is not a palindrome")
