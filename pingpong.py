90e58aa5e89f19126c1021a161bb1d0b530cc148
Varun Nivas <froztedfate@gmail.com>
2018-10-31 22:27:17 +0530

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -83,11 +83,3 @@ while True:
 	elif ball.xcor() < -350:
 		ball.goto(0, 0)
 		ball.dx *= -1
-	if (ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50):
-		ball.dx *= -1
-	elif (ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
-		ball.dx *= -1
-
-
-
-
